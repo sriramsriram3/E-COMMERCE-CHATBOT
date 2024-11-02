8c99a8005e30d5b7b91d11571bad854c846f27bb
simhadri <simhadrisriram3@gmail.com>
2024-11-02 20:49:17 +0530

    setup done

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup,find_packages
+
+setup(name="flipkart",
+      version='0.0.1',
+      description='it is a function used for flipkart recommendation system',
+      author='simhadrisriram',
+      author_email='simhadrisriram3@gmail.com',
+      packages=find_packages())
